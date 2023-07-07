9f9cea0f9a753b0795f539937ec03ff8f2574b5f
Your Name <prajapatirajat94@gmail.com>
2023-07-06 23:39:04 -0400

    7july2023

--- src/test/java/JSonDataTransfer/JsonFileDataTransfer.java
+++ src/test/java/JSonDataTransfer/JsonFileDataTransfer.java
@@ -12,17 +12,38 @@ public class JsonFileDataTransfer {
 	public static void main(String[] args) throws IOException {
                     ObjectMapper  Ob_MP =  new ObjectMapper();
                   JsonNode node=  Ob_MP.readTree(new File("C:\\Users\\praja\\eclipse-workspace\\"
-                    		+ "Javalearning\\src\\test\\java\\JSonDataTransfer\\ONB.json"));
+                    		+ "Javalearning\\src\\test\\java\\JSonDataTransfer\\color.json"));
                   
-                  System.out.println(node.path(0).findPath("cars").size());
-                  int s=node.path(0).findPath("cars").size();
-                  System.out.println(s);
-                  for(int i=0;i<s;i++) {
-                	  System.out.println(node.path(0).findPath("cars").get(i).asText());
+                 // System.out.println(node.path(0).findPath("cars").size());
+                  JsonNode AddNode=    node.path("ClientProfile").
+                		  findPath("Clients").
+                		  findPath("Client1")
+                		  .findValue("Address");
+                  for(int i=0;i<AddNode.size();i++) {
+                	 String val= AddNode.get(i).asText();
+                	 System.out.println(val);
                   }
-              
-
-
+                  JsonNode boD_Node=node.path("ClientProfile").path("Clients").path(0).path("Client2");//.path("Client1");
+                  System.out.println(boD_Node);
+                  DOB_VAL_From_Node(boD_Node);
+//                  System.out.println( node.path("ClientProfile").path("AccountType").asText());
+//                 
+                  
+                  
+	}
+	
+	public static void DOB_VAL_From_Node(JsonNode nod) {
+		
+		System.out.println(nod.path("DOB").findPath("Date").asText());
+		System.out.println(nod.path("DOB").findPath("Month").asText());
+		System.out.println(nod.path("DOB").findPath("year").asText());
+		
+//		System.out.println(nod.path("DOB").path("Month").asText());
+//		System.out.println(nod.path("DOB").path("year").asText());
+		//nod.path("DOB").path("year").asText();
+		
+		
+		
 	}
 
 }
