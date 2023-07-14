bc74f0a93747b1d597afb177fca076f32a11946b
Your Name <prajapatirajat94@gmail.com>
2023-07-13 22:19:39 -0400

    13july2023

--- src/test/java/MavenParameterPass/MvnPractice.java
+++ src/test/java/MavenParameterPass/MvnPractice.java
@@ -14,18 +14,24 @@ public class MvnPractice {
     mvn -Dbrowser=chrome -Dweb=Rajat.com clean install 
     3.you can pass data to pom.xml file as well cover data with ${file} for xml file
     mvn -Dbrowser=chrome -Dweb=Rajat.com -Dfile=file1 clean install 
-    4.if this browser properties available in pom.xml file then priority will be given to pom properties value
+    4.
+    case1: run from local:-
+    if this browser properties value available in pom.xml file then priority will be given to pom properties value
+    case2. run from maven:-if parameter avaialble in .xml file then priority will be given to .xml file
+         and system value will come through maven->pom.xml variables. and override pom.xml value
+    
     
  
 */
 	
    @Test
-   @Parameters({"browser","web"})
-	public void Testing(@Optional String browser,@Optional String WEb) {
+   @Parameters({"BROWSER","WEB"})
+	public void Testing(@Optional String browser,@Optional String Web) {
 	   
 	   System.out.println("browser value is : ===>> "+browser);
-	   System.out.println("web value is : ===>> "+WEb);
+	   System.out.println("web value is : ===>> "+Web);
 	   System.out.println("POM browser value is : ===>> "+System.getProperty("browser"));
+	   System.out.println("POM web value is : ===>> "+System.getProperty("web"));
 	   
 	   
 		
